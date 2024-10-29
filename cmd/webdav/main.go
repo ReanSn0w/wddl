14cd08b83d83c03788d1bb39e807b7f053d4c138
Dmitriy Papkov <papkovda@me.com>
2024-10-29 15:20:26 +0000

    [Update] main.go

--- cmd/webdav/main.go
+++ cmd/webdav/main.go
@@ -19,10 +19,11 @@ var (
 		User     string `short:"u" long:"user" env:"USER" default:"guest" description:"webdav user"`
 		Password string `short:"p" long:"password" env:"PASSWORD" description:"webdav password"`
 
-		Input   string `short:"i" long:"input" env:"INPUT" description:"input path"`
-		Output  string `short:"o" long:"output" env:"OUTPUT" description:"output path"`
-		Threads int    `long:"threads" env:"THREADS" default:"5" description:"parallel downloads"`
-		Sync    bool   `long:"sync" env:"SYNC" description:"sync mode will delete local files when it deleted on remote webdav storage"`
+		Input    string `short:"i" long:"input" env:"INPUT" description:"input path"`
+		Output   string `short:"o" long:"output" env:"OUTPUT" description:"output path"`
+		Threads  int    `long:"threads" env:"THREADS" default:"5" description:"parallel downloads"`
+		Sync     bool   `long:"sync" env:"SYNC" description:"sync mode will delete local files when it deleted on remote webdav storage"`
+		Interval int    `long:"interval" env:"INTERVAL" default:"3" description:"sync retry interval. [default: 3] eq 3 seconds"`
 	}{}
 )
 
@@ -37,7 +38,7 @@ func main() {
 	}
 
 	s := sync.New(app.Log(), wd, opts.Threads, opts.Input, opts.Output)
-	s.Start(app.Context(), time.Second*10)
+	s.Start(app.Context(), time.Second*time.Duration(opts.Interval))
 	app.Add(func(ctx context.Context) {
 		s.Stop()
 	})
